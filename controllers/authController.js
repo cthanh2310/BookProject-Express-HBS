0fb139d61711ec29f60db7951ae5aeec3f367b8a
unknown <thanhpham.23102000@gmail.com>
2021-06-28 15:28:25 +0700

    Login + register : Done!

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,14 @@
 class authController{
+    login(req, res, next){
+        res.render('login', {layout: 'auth'});
+    }
+    register(req, res, next){
+        res.render('register', {layout: 'auth'});
+    }
     auth(req, res, next){
         res.render('auth');
     }
+    
 }
 
 module.exports = new authController();
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController.js');
 
+router.get('/login', authController.login);  // auth/login
+router.get('/register', authController.register);  // auth/register
 router.get('/', authController.auth);
-
 module.exports = router;
