d9408e15f4defceffb9b11b2fe9092c9a4f240f0
unknown <thanhpham.23102000@gmail.com>
2021-07-06 11:21:30 +0700

    generate environmentn variable + connect DB success. Bug hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,29 @@
-class authController{
-    login(req, res, next){
-        res.render('login', {layout: 'auth'});
+const users = require('../models/user');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+class authController {
+    async login(req, res, next) {
+
+        res.render('login', { layout: 'auth' });
     }
-    register(req, res, next){
-        res.render('register', {layout: 'auth'});
+    async register_post(req, res, next) {
+        try {
+            const user = await users.create(req.body);
+            const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
+            res.status(200).json({
+                status: 'success',
+                data: { token, email: user.email }
+            })
+
+        } catch (error) {
+            next(error);
+        }
     }
-    auth(req, res, next){
-        res.render('auth');
+    async register(req, res, next) {
+        res.render('register', { layout: 'auth' });
     }
-    
+
 }
 
 module.exports = new authController();
